574f4ad46aea46232992a4c23c4db2eb990b5d2d
Promie Yutasane <promie.yutasane@gmail.com>
2024-07-25 03:41:20 +1000

    SBC-06 completed the createBlogPost handler with uuid and proper typing

--- src/createBlogPost.ts
+++ src/createBlogPost.ts
@@ -1,13 +1,29 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { v4 as uuid } from 'uuid'
 import { IBlogPost } from './types'
+
+interface ICreateBlogPostRequest {
+  title: string
+  author: string
+  content: string
+}
 
 export const handler = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
-  const partialBlogPost = JSON.parse(event.body || '{}') as Partial<IBlogPost>
+  const partialBlogPost = JSON.parse(event.body!) as ICreateBlogPostRequest
+
+  const id = uuid()
+  const createdAt = new Date().toISOString()
+
+  const blogPost: IBlogPost = {
+    id,
+    createdAt,
+    ...partialBlogPost,
+  }
 
   return {
     statusCode: 201,
-    body: JSON.stringify(partialBlogPost),
+    body: JSON.stringify(blogPost),
   }
 }
