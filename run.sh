fc92cc4be02a202ee870192b8f32eab002abf54c
Promie Yutasane <promie.yutasane@gmail.com>
2024-07-24 10:53:04 +1000

    SBC-01 installed prettier and eslint dependencies

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 cdk init --language typescript
 
+# ESLint and Prettier packages
+npm i -D @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier eslint-config-standard-with-typescript eslint-plugin-import eslint-plugin-n eslint-plugin-promise prettier
+
 export AWS_PROFILE=promie
