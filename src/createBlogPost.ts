a4be3dc0bc10d9f78abac30047c48e7be6775122
Promie Yutasane <promie.yutasane@gmail.com>
2024-07-25 03:31:02 +1000

    SBC-06 updated the createBlogPost to return partialBlogPost from the event body

--- src/createBlogPost.ts
+++ src/createBlogPost.ts
@@ -1,13 +1,13 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { IBlogPost } from './types'
 
 export const handler = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
+  const partialBlogPost = JSON.parse(event.body || '{}') as Partial<IBlogPost>
+
   return {
     statusCode: 201,
-    body: JSON.stringify({
-      message: 'Hello World',
-      event: JSON.stringify(event),
-    }),
+    body: JSON.stringify(partialBlogPost),
   }
 }
