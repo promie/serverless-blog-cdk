8f13c15d06383e15a257cd94b003a1813d46b564
Promie Yutasane <promie.yutasane@gmail.com>
2024-07-24 12:38:09 +1000

    SBC-04 added aws gateway and defined the POST route

--- lib/serverless-blog-cdk-stack.ts
+++ lib/serverless-blog-cdk-stack.ts
@@ -1,9 +1,17 @@
-import { aws_lambda_nodejs, Stack, StackProps } from 'aws-cdk-lib'
+import {
+  aws_lambda_nodejs,
+  aws_apigateway,
+  Stack,
+  StackProps,
+} from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 
 export class ServerlessBlogCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
+
+    const api = new aws_apigateway.RestApi(this, 'blogPostApi')
+
     const createBlogPostLambdaName = 'createBlogPostHandler'
     const createBlogPostLambda = new aws_lambda_nodejs.NodejsFunction(
       this,
@@ -15,6 +23,11 @@ export class ServerlessBlogCdkStack extends Stack {
       },
     )
 
-    console.info(createBlogPostLambda)
+    const blogPostPath = api.root.addResource('blogposts')
+
+    blogPostPath.addMethod(
+      'POST',
+      new aws_apigateway.LambdaIntegration(createBlogPostLambda),
+    )
   }
 }
