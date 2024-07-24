86bec2d3cfcd089a7ce5b74d432da111382e2f0b
Promie Yutasane <promie.yutasane@gmail.com>
2024-07-24 14:18:44 +1000

    SBC-04 extracted the createLambda into a separate function

--- lib/blog-post-handler.ts
+++
@@ -1,8 +0,0 @@
-export const createBlogPostHandler = async () => {
-  return {
-    statusCode: 201,
-    body: JSON.stringify({
-      message: 'Hello World',
-    }),
-  }
-}
--- lib/serverless-blog-cdk-stack.ts
+++ lib/serverless-blog-cdk-stack.ts
@@ -1,33 +1,39 @@
-import {
-  aws_lambda_nodejs,
-  aws_apigateway,
-  Stack,
-  StackProps,
-} from 'aws-cdk-lib'
+import * as cdk from 'aws-cdk-lib'
+import { Cors, LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 import { Construct } from 'constructs'
 
-export class ServerlessBlogCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class ServerlessBlogCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    const api = new aws_apigateway.RestApi(this, 'blogPostApi')
-
-    const createBlogPostLambdaName = 'createBlogPostHandler'
-    const createBlogPostLambda = new aws_lambda_nodejs.NodejsFunction(
-      this,
-      createBlogPostLambdaName,
-      {
-        entry: 'lib/blog-post-handler.ts',
-        handler: createBlogPostLambdaName,
-        functionName: createBlogPostLambdaName,
+    // API
+    const blogsApi = new RestApi(this, 'blogPostApi', {
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
       },
+    })
+
+    const blogsResource = blogsApi.root.addResource('blogs')
+
+    // Function
+    const createBlogPostFunction = this.createLambda(
+      'createBlogPost',
+      'src/createBlogPost.ts',
     )
-
-    const blogPostPath = api.root.addResource('blogposts')
-
-    blogPostPath.addMethod(
+    blogsResource.addMethod(
       'POST',
-      new aws_apigateway.LambdaIntegration(createBlogPostLambda),
+      new LambdaIntegration(createBlogPostFunction),
     )
+  }
+
+  createLambda = (name: string, path: string) => {
+    return new NodejsFunction(this, name, {
+      functionName: name,
+      runtime: Runtime.NODEJS_18_X,
+      entry: path,
+    })
   }
 }
